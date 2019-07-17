133dfd60cae5a27c008f06edc5e3291e4e70599d
lpile <logan.pile@gmail.com>
2019-07-16 23:14:10 -0600

    Add visitor can search landmarks spec

---
+++ spec/features/visitor/visitor_can_search_by_location_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+feature 'Visitor can search landmarks' do
+  scenario 'with name of city' do
+    VCR.use_cassette('visitor_search_by_city') do
+      visit landmarks_path
+
+      fill_in :query, with: 'Denver'
+      click_on 'Search'
+
+      expect(current_path).to eq('/landmarks')
+
+      within(first('.landmark_results')) do
+        expect(page).to have_css('.landmark_name')
+        expect(page).to have_css('.landmark_photos')
+        expect(page).to have_css('.landmark_rating')
+        expect(page).to have_css('.landmark_types')
+        expect(page).to have_css('.landmark_lat')
+        expect(page).to have_css('.landmark_lng')
+        expect(page).to have_css('.landmark_place_id')
+      end
+    end
+  end
+end
+
+# As a visitor / user / admin,
+# When I visit the landmark index,
+# I see a map of the area around my IP address’ location with pins for landmarks (??)
+# I see a box for me to enter the search location.
+# When I enter a location (zip code, address, etc),
+# And I click “Search”,
+# I see a map with pins for landmarks around the desired location.
