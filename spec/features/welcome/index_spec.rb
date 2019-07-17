7731c07b2696f13db37aa81e63a9871b48fef3a5
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-17 15:18:23 -0600

    Add basic content to welcome index

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe "Welcome Index" do
       expect(current_path).to eq(root_path)
       expect(status_code).to eq(200)
     end
+
+    it 'shows basic info about the site' do
+      expect(page).to have_content('Welcome to Oddio')
+      # TODO: Issue #16 - Add brief description of the site & sample content
+      expect(page).to have_link('Enter')
+      expect(page).to have_selector(:css, "a[href=\"#{landmarks_path}\"]")
+    end
   end
 end
