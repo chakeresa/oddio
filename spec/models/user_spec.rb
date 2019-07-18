d1bd9c402de23a70931d2d648b75fcfd022cb199
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-17 23:09:56 -0600

    Add test that default user is a regular user (not an admin)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it {should validate_presence_of :username}
+    it { should validate_uniqueness_of(:username).case_insensitive }
     it {should validate_presence_of :role}
     it {should validate_presence_of :password_digest}
+  end
+
+  it 'defaults to a regular (non-admin) user' do
+    user = User.create(username: 'bobby', password: 'password')
+    expect(user.role).to eq('user')
   end
 end
