4301524336c6d7f9e26c45c74247762e13149e51
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-17 21:29:00 -0600

    Add user_id to session upon registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome, #{@user.username}!"
+      session[:user_id] = @user.id
       redirect_to landmarks_path
     else
-      flash[:danger] = 'Some error message'
+      flash[:danger] = @user.errors.full_messages.join('. ')
       render :new
     end
   end
