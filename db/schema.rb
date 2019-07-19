e3df841726cc27d3acb62debb66182d922f257d4
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 13:20:21 -0600

    changes schema to add recordings

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_230931) do
+ActiveRecord::Schema.define(version: 2019_07_18_205626) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recordings", force: :cascade do |t|
+    t.string "title"
+    t.text "url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recordings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +31,6 @@ ActiveRecord::Schema.define(version: 2019_07_17_230931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "recordings", "users"
 end
