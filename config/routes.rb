ba0d2822fe15212b02c21e2e67bb8f5dce17e81f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 13:20:04 -0600

    adds recordings index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see 
+  # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+
+  resources :recordings, only: [:index]
 
   resources :landmarks, only: [:index]
 end
