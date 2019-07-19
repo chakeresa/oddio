8d8dbf43086f590d3e69be4cb3b9e3f74e652584
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 13:20:54 -0600

    adds aws_service_spec

--- spec/services/aws_service_spec.rb
+++ spec/services/aws_service_spec.rb
@@ -3,15 +3,19 @@
 require 'rails_helper'
 
 describe AWS_Service do
-  describe 'recordings' do
-    it "test" do
-      WebMock.disable!
+  it "has expected contents" do
+    VCR.use_cassette('AWS_service_spec') do
+      aws_service = AWS_Service.new
 
-      something = AWS_Service.new
-
-      binding.pry
-      expect(something.get_recordings.class).to eq()
-
+      # should as of this test have 3 objects in S3 Bucket 'oddio1903'
+      expect(aws_service.get_recordings.class)
+        .to eq(Seahorse::Client::Response)
+      expect(aws_service.get_recordings.contents.class)
+        .to eq(Aws::Xml::DefaultList)
+      expect(aws_service.get_recordings.contents.first.class)
+        .to eq(Aws::S3::Types::Object)
+      expect(aws_service.get_recordings.contents.count)
+        .to eq(3)
     end
   end
 end
