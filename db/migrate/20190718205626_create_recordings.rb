e04d03d4461db33f3c5862a9d6d3e8efab9bde0d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 13:20:13 -0600

    adds recordings migration

--- db/migrate/20190718205626_create_recordings.rb
+++ db/migrate/20190718205626_create_recordings.rb
@@ -2,9 +2,8 @@ class CreateRecordings < ActiveRecord::Migration[5.2]
   def change
     create_table :recordings do |t|
       t.string :title
-      t.string :url
+      t.text :url
       t.references :user, foreign_key: true
-      t.references :landmark, foreign_key: true
 
       t.timestamps
     end
