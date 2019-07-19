c3c82e6d0541b7e4238b56a77f98ddd1ed48252a
lpile <logan.pile@gmail.com>
2019-07-19 13:48:35 -0600

    Hotfix issues

--- app/serializers/landmark_serializer.rb
+++ app/serializers/landmark_serializer.rb
@@ -1,4 +1,4 @@
 class LandmarkSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :lat, :long, :address, :category, :photo_url, :phone, :website
+  attributes :name, :lat, :long, :address, :category, :phone_number
 end
--- spec/factories/landmarks.rb
+++ spec/factories/landmarks.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     sequence(:long) { |n|-104.9903 + n * 0.0015}
     sequence(:address) { |n| "Address #{n}" }
     category { "park" }
-    sequence(:website) { |n| "Website #{n}" }
-    sequence(:phone) { |n| "Phone #{n}" }
-    sequence(:photo_url) { |n| "Photo Url #{n}" }
+    sequence(:phone_number) { |n| "Phone Number #{n}" }
   end
 end
