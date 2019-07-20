fdec2283d92bcbe77664a3396ace827ae77aa26d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 19:20:58 -0600

    implements running create method to recordings controller
    Co-Authored-By: Alexandra Chakeres <alexchakeres@gmail.com>

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -12,11 +12,23 @@ class RecordingsController < ApplicationController
     # binding.pry
     # file = File.open("app/assets/new_recording_1.mp3")
     # s3.put_object(bucket: 'oddio1903', key: 'file_1.mp3', body: file)
-
     file = params[:recording][:recording]
+    file_name = "#{Time.now.to_s}-#{file.original_filename}"
     s3 = Aws::S3::Client.new(profile: 'oddio1903', region: 'us-east-2')
-    s3.put_object(bucket: 'oddio1903', key: "#{Time.now.to_s}-#{file.original_filename}", body: file)
-    #add recording obj to db
+    s3.put_object(bucket: 'oddio1903', key: file_name, body: file)
+    recording = Recording.new(
+      user: current_user,
+      landmark_id: params[:landmark_id].to_i,
+      url: "https://#{ENV['AWS_BUCKET']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/#{file_name}",
+      title: params[:recording][:title]
+    )
+    if recording.save!
+      flash[:success] = "Your recording has been saved."
+      redirect_to landmark_path(params[:landmark_id])
+    else
+      flash[:error] = "Unable to save recording."
+      render :new
+    end
   end
 
 
