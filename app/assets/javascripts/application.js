d7e16629600df26b8d2b22f29b7ac74f6e21cac8
Patrick Duvall <duvallp208@gmail.com>
2019-07-21 11:34:10 -0600

    change gear point, currently trying to refactor to walkign and remove route

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
+//= require_tree .
 //= require activestorage
 //= require_tree .
 //= require  leaflet/dist/leaflet.js
