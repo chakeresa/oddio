f52205200afa7609c7126d91b2e94df74b39cc38
Patrick Duvall <duvallp208@gmail.com>
2019-07-21 17:35:25 -0600

    Resolve issues from merging in Geocoder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require jquery_ujs
 //= require_tree .
 //= require activestorage
 //= require_tree .
