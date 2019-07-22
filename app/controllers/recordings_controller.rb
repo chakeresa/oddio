d63a7f3f2f6948dbdf48e7e2e2d7b6bff861f4c0
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-22 08:54:05 -0600

    slight changes to view, working on getting recordings to work again

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -4,32 +4,38 @@ class RecordingsController < ApplicationController
   end
 
   def new
-    landmark = Landmark.find(params[:landmark_id])
-    @recording = landmark.recordings.new
+    if current_user
+      landmark = Landmark.find(params[:landmark_id])
+      @recording = landmark.recordings.new
+    else
+      flash.now[:error] = "You must be logged in to make a recording."
+      redirect_to landmark_path(landmark))
+    end
   end
 
   def create
-    # binding.pry
-    # file = File.open("app/assets/new_recording_1.mp3")
-    # s3.put_object(bucket: 'oddio1903', key: 'file_1.mp3', body: file)
-    file = params[:recording][:recording]
-    ran = SecureRandom.hex
-    file_name = "#{ran}.mp3"
-    binding.pry
-    s3 = Aws::S3::Client.new(profile: 'oddio1903', region: 'us-east-2')
-    s3.put_object(bucket: 'oddio1903', key: file_name, body: file)
-    recording = Recording.new(
-      user: current_user,
-      landmark_id: params[:landmark_id].to_i,
-      url: "https://#{ENV['AWS_BUCKET']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/#{file_name}",
-      title: params[:recording][:title]
-    )
-    if recording.save!
-      flash[:success] = "Your recording has been saved."
-      redirect_to landmark_path(params[:landmark_id])
+    if current_user
+      file = params[:recording][:recording]
+      ran = SecureRandom.hex
+      file_name = "#{ran}.mp3"
+      s3 = Aws::S3::Client.new(profile: 'oddio1903', region: 'us-east-2')
+      s3.put_object(bucket: 'oddio1903', key: file_name, body: file)
+      recording = Recording.new(
+        user: current_user,
+        landmark_id: params[:landmark_id].to_i,
+        url: "https://#{ENV['AWS_BUCKET']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/#{file_name}",
+        title: params[:recording][:title]
+      )
+      if recording.save!
+        flash[:success] = "Your recording has been saved."
+        redirect_to landmark_path(params[:landmark_id])
+      else
+        flash.now[:error] = "Unable to save recording."
+        render :new
+      end
     else
-      flash.now[:error] = "Unable to save recording."
-      render :new
+      flash[:error] = "You must be logged in to make a recording."
+      redirect_to landmark_path(Landmark.find(params[:landmark_id]))
     end
   end
 
