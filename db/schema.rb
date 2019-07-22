4da5238066a2d84949938388fac37fa1fcc2cb8e
lpile <logan.pile@gmail.com>
2019-07-22 16:03:53 -0600

    Add md5_hash to landmarks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_031155) do
+ActiveRecord::Schema.define(version: 2019_07_22_215652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_031155) do
     t.string "photo_reference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "md5_hash"
   end
 
   create_table "recordings", force: :cascade do |t|
