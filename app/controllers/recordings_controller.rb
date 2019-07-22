c4eb2ff6d113159b36083d78e26972fa8041026f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-22 14:51:13 -0600

    user namespacing of new/create recordings

--- app/controllers/recordings_controller.rb
+++ app/controllers/recordings_controller.rb
@@ -2,41 +2,4 @@ class RecordingsController < ApplicationController
   def index
     @recordings = Recording.all
   end
-
-  def new
-    landmark = Landmark.find(params[:landmark_id])
-    @recording = landmark.recordings.new
-    if !current_user
-      flash.now[:error] = "You must be logged in to make a recording."
-      redirect_to landmark_path(landmark)
-    end
-  end
-
-  def create
-    if current_user
-      file = params[:recording][:recording]
-      ran = SecureRandom.hex
-      file_name = "#{ran}.mp3"
-      s3 = Aws::S3::Client.new(profile: 'oddio1903', region: 'us-east-2')
-      s3.put_object(bucket: 'oddio1903', key: file_name, body: file)
-      recording = Recording.new(
-        user: current_user,
-        landmark_id: params[:landmark_id].to_i,
-        url: "https://#{ENV['AWS_BUCKET']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/#{file_name}",
-        title: params[:recording][:title]
-      )
-      if recording.save!
-        flash[:success] = "Your recording has been saved."
-        redirect_to landmark_path(params[:landmark_id])
-      else
-        flash.now[:error] = "Unable to save recording."
-        render :new
-      end
-    else
-      flash[:error] = "You must be logged in to make a recording."
-      redirect_to landmark_path(Landmark.find(params[:landmark_id]))
-    end
-  end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see
-  # http://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
 
   get '/register', to: 'app_auths#new'
   resources :app_auths, only: [:create]
   resources :users, only: [:new, :create]
-  
+
   get '/auth/google_oauth2/callback', to: 'google_auths#create'
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  resources :landmarks, only: [:index, :show] do
-    resources :recordings, only: [:new, :create]
-
-  end
+  resources :landmarks, only: [:index, :show]
 
   resources :recordings, only: [:index]
+
+  namespace :user do
+    resources :landmarks, only: [:show] do
+      resources :recordings, only: [:new, :create]
+    end
+  end
 
   namespace :api do
     namespace :v1 do
