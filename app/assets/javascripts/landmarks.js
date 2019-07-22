d5ed3c9a1055dcebd856030e07a9c023eb3aff23
lpile <logan.pile@gmail.com>
2019-07-22 15:39:02 -0600

    Format landmark show template

--- app/assets/javascripts/landmarks.js
+++ app/assets/javascripts/landmarks.js
@@ -2,7 +2,7 @@ function initMap(lat, lng) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
     center: myCoords,
-    zoom: 16
+    zoom: 17
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
     var marker = new google.maps.Marker({
