94da20ad8daa2713c901d74068449e7f268d8120
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-24 15:53:29 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  # only allow https
-
+  
   # config.assets.compile = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
