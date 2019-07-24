7603cef3b847242073f4c3e17d41758b3c17240f
Patrick Duvall <duvallp208@gmail.com>
2019-07-24 09:34:54 -0600

    Add markers for government buildings and parks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,6 +31,16 @@ var greenIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
+
+var greyIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 //<!--make purple markers-->
 var violetIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
