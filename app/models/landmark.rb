04704de1454dd0782b976d7095252db6e725b816
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-24 11:55:53 -0600

    Changes to landmark model due to master pull

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -3,27 +3,6 @@ class Landmark < ApplicationRecord
   validates_presence_of :lat, :long, :name, :address, :category, :photo_reference
   validates :place_id, presence: true, uniqueness: true
 
-<<<<<<< HEAD
-  before_save :update_md5_hash
-  validates_presence_of :lat, :long, :name, :address, :category
-
-
-  private
-    def update_md5_hash
-      self.md5_hash = Digest::MD5.hexdigest(
-        "#{self.name}
-        #{self.place_id}
-        #{self.address}
-        #{self.phone_number}
-        #{self.category}
-        #{self.website}
-        #{self.photo_reference}"
-      )
-    end
-=======
-  validates_presence_of :lat, :long, :name, :address, :category, :photo_reference
-  validates :place_id, presence: true, uniqueness: true
-
   before_save :update_md5_hash
 
   private
@@ -39,5 +18,4 @@ class Landmark < ApplicationRecord
       #{self.photo_reference}"
     )
   end
->>>>>>> 75b46aee16f8431b4b28d085c1b4ff06652e9983
 end
