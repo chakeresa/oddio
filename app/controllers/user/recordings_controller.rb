c55ee963d1a722dc796644cc3b6531d94366f96b
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-24 20:15:55 -0600

    adds content_type header to s3 call

--- app/controllers/user/recordings_controller.rb
+++ app/controllers/user/recordings_controller.rb
@@ -7,9 +7,14 @@ class User::RecordingsController < User::BaseController
   def create
     file = params[:recording][:recording]
     ran = SecureRandom.hex
-    file_name = "#{ran}.mp3"
+    file_name = "#{ran}"
     s3 = Aws::S3::Client.new(profile: 'oddio1903', region: 'us-east-2')
-    s3.put_object(bucket: 'oddio1903', key: file_name, body: file)
+    s3.put_object(
+      bucket: 'oddio1903',
+      key: file_name,
+      body: file,
+      content_type: "audio/mpeg"
+    )
     recording = Recording.new(
       user: current_user,
       landmark_id: params[:landmark_id].to_i,
