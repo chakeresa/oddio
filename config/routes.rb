ca46f0ba5a80297ff5b71bce5c1cfa3deaafbcb8
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-24 19:12:34 -0600

    Add feature test of landmark show page for a tour

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
-  # Registration & Login
+  ### Registration & Login
   get '/register', to: 'app_auths#new'
   resources :app_auths, only: [:create]
   resources :users, only: [:new, :create, :show]
@@ -11,24 +11,27 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  ### Public
   # Landmarks
   resources :landmarks, only: [:index, :show]
-
-  namespace :user do
-    resources :landmarks, only: [:show] do
-      resources :recordings, only: [:new, :create]
-    end
-  end
 
   namespace :api do
     namespace :v1 do
       resources :landmarks, only: [:index, :show]
     end
   end
-
+  
   # Recordings
   resources :recordings, only: [:index]
-
+  
   # Tours
   resources :tours, only: [:index, :show]
+  get '/tours/:tour_id/landmarks/:id', to: 'tour/landmarks#show', as: 'tour_landmark'
+  
+  ### User
+  namespace :user do
+    resources :landmarks, only: [:show] do
+      resources :recordings, only: [:new, :create]
+    end
+  end
 end
