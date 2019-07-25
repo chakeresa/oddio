81085df0fba32c21d9898819a8f2b4c4165e39d3
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-25 09:27:44 -0600

    seeds, update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
 user_1 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_2 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_2 = User.create(email: "severly@nlspam.cf", first_name: "Benjamin", last_name: "Clementine", display_name: "benjiboio")
+AppAuth.create(user: user, username: "benjiboio", password: "password")
+
+user_3 = User.create(email: "glaaroussi.ouahbn@elbow4.tk", first_name: "Vanya", last_name: "Mikelretch", display_name: "Filandrothrope")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_3 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_4 = User.create(email: "etlem@bzymail.top", first_name: "Jefferson", last_name: "Whitehead", display_name: "GrassEater")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_4 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_5 = User.create(email: "wabhifb07v@myhagiasophia.com", first_name: "Frank", last_name: "Brandswater", display_name: "fbrandswater")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_5 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_6 = User.create(email: "ejakub.k@domainaing.cf", first_name: "Grace", last_name: "Predente", display_name: "HolyGrace")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_6 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_7 = User.create(email: "7wanderson.silza@mmspa.cf", first_name: "Selma", last_name: "Graniour", display_name: "SelmySelms")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_7 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_8 = User.create(email: "basdsx98o@apagitu.info.tm", first_name: "Persephone", last_name: "Orelia", display_name: "perseophoneo")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_8 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_9 = User.create(email: "hoblas7@emailibre.com", first_name: "Matthew", last_name: "Merchant", display_name: "MMerchant1287")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
-user_9 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
-AppAuth.create(user: user, username: "Nunyanar", password: "password")
-
-user_10 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_10 = User.create(email: "4zianiziani.2@ezvsmk.tk", first_name: "Phoebe", last_name: "Halliwell", display_name: "powerothree")
 AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
 landmark = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
