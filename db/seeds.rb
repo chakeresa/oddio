6291399b6d681f07166b71820d64f659f9587468
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-25 09:13:35 -0600

    creates seeds for recordings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,95 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+user_1 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_2 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_3 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_4 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_5 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_6 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_7 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_8 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_9 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
+
+user_10 = User.create(email: "shmeeshmail@yahoo.com", first_name: "Nunya", last_name: "Business", display_name: "Nar")
+AppAuth.create(user: user, username: "Nunyanar", password: "password")
 
 landmark = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
+opera_house = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
+museum = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
+axe_room = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
+art_gallery = Landmark.create(lat: 39.7392358, long: -104.990251, name: "Cool Place", address: "123 Place", phone_number: "867-5309", category: "Museum")
 
 recording = Recording.create(title: "Place is RAD", url: "https://www.botanicgardens.org/", user: user, landmark: landmark)
+
+Recording.create(title: '"Don Giovanni"',
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+
+Recording.create(title: "I hate my job",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "I hate my job",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
+Recording.create(title: "Don Giovanni",
+                 url: "https://oddio1903.s3.us-east-2.amazonaws.com/a74401d47b4d40de7760cc73b5449659.mp3",
+                 user: user,
+                 landmark: )
