b8fa998969f4882fc0515b48aa4d6a1da12d24f9
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-24 19:32:16 -0600

    Adds cassettes to make rspec pass

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -6,27 +6,32 @@ feature 'user show page' do
       @user = create(:user)
       create_list(:recording, 2, user: @user)
 
-      visit user_path(@user)
     end
 
     it 'shows all recordings for one content creator' do
-      expect(page).to have_content(@user.display_name)
-      expect(page.all('.recording-list').count).to eq(2)
+      VCR.use_cassette('visitor_sees_recordings_for_creator', record: :new_episodes) do
+        visit user_path(@user)
+        expect(page).to have_content(@user.display_name)
+        expect(page.all('.recording-list').count).to eq(2)
 
-      within(first('.recording-list')) do
-        recording = @user.recordings.first
-        expect(page).to have_content(recording.title)
-        expect(page).to have_link(recording.landmark.name, href: landmark_path(recording.landmark))
-        expect(page.all('audio').count).to eq(1)
+        within(first('.recording-list')) do
+          recording = @user.recordings.first
+          expect(page).to have_content(recording.title)
+          expect(page).to have_link(recording.landmark.name, href: landmark_path(recording.landmark))
+          expect(page.all('audio').count).to eq(1)
+        end
       end
     end
 
     it 'shows a message if there are no recordings' do
-      user = create(:user)
-      visit user_path(user)
+      VCR.use_cassette('visitor_sees_message_for_no_recordings', record: :new_episodes) do
+        visit user_path(@user)
+        user = create(:user)
+        visit user_path(user)
 
-      expect(page).to have_content('Recordings')
-      expect(page).to have_content('No recordings uploaded yet')
+        expect(page).to have_content('Recordings')
+        expect(page).to have_content('No recordings uploaded yet')
+      end
     end
   end
 end
