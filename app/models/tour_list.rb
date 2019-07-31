b815541dd99c8d34e1108ac0474ca3c114bb7324
lpile <logan.pile@gmail.com>
2019-07-31 16:59:41 -0600

    Add TourList class

---
+++ app/models/tour_list.rb
@@ -0,0 +1,25 @@
+class TourList
+  attr_reader :contents
+
+  def initialize(initial_contents)
+    @contents = initial_contents || Hash.new(0)
+    @contents.default = 0
+  end
+
+  def total_count
+    @contents.values.sum
+  end
+
+  def count_of(landmark_id)
+    @contents[landmark_id.to_s]
+  end
+
+  def add_landmark(landmark_id)
+    @contents[landmark_id.to_s] += 1
+  end
+
+  def remove_landmark(landmark_id)
+    @contents[landmark_id.to_s] -= 1
+    @contents.delete(landmark_id.to_s) if count_of(landmark_id) == 0
+  end
+end
