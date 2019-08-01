daac3c4a0bec6c465292d35e5784945a816e1be9
lpile <logan.pile@gmail.com>
2019-07-31 21:28:13 -0600

    Refactor specs

--- spec/features/tour_list/create_spec.rb
+++ spec/features/tour_list/create_spec.rb
@@ -1,61 +1,97 @@
 require 'rails_helper'
 
-RSpec.describe "Tourlist Create", vcr: :new_episodes do
-  describe 'as a user' do
-    before(:each) do
-      @user = create(:user)
-      @landmark1 = create(:landmark)
-      @landmark2 = create(:landmark)
-      @landmark3 = create(:landmark)
+RSpec.describe "tour list create", vcr: { :record => :new_episodes }, type: :feature do
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  let(:user) { create(:user) }
+  let(:landmark1) { create(:landmark) }
+  let(:landmark2) { create(:landmark) }
+  let(:landmark3) { create(:landmark) }
+
+  context 'as a visitor' do
+    scenario 'unable to see create tour display' do
+      visit landmarks_path
+
+      expect(page).to_not have_content('Create Tour(0)')
+    end
+  end
+
+  context 'as a user' do
+    before :each do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     end
 
-    it 'displays a message when you add landmark to tour list' do
-      visit landmark_path(@landmark1)
+    scenario 'displays a message when you add landmark to tour list' do
+      visit landmark_path(landmark1)
 
-      within(".attributes") do
-        click_button "Add to Tour List"
+      within('.attributes') do
+        click_button 'Add to Tour List'
       end
 
-      expect(page).to have_content("You have added #{@landmark1.name} to your tour list.")
+      expect(page).to have_content("You have added #{landmark1.name} to your tour list.")
     end
 
-    it 'can add multiple landmarks to tour list' do
-      visit landmark_path(@landmark1)
-      click_button "Add to Tour List"
-      expect(page).to have_content("You have added #{@landmark1.name} to your tour list.")
-      expect(page).to have_content("You have 1 landmarks in your tour list.")
+    scenario 'can add multiple landmarks to tour list' do
+      visit landmark_path(landmark1)
+      
+      click_button 'Add to Tour List'
 
-      visit landmark_path(@landmark2)
-      click_button "Add to Tour List"
-      expect(page).to have_content("You have added #{@landmark2.name} to your tour list.")
-      expect(page).to have_content("You have 2 landmarks in your tour list.")
+      expect(page).to have_content("You have added #{landmark1.name} to your tour list.")
+      expect(page).to have_content('You have 1 landmarks in your tour list.')
 
-      visit landmark_path(@landmark3)
-      click_button "Add to Tour List"
-      expect(page).to have_content("You have added #{@landmark3.name} to your tour list.")
-      expect(page).to have_content("You have 3 landmarks in your tour list.")
+      visit landmark_path(landmark2)
+
+      click_button 'Add to Tour List'
+
+      expect(page).to have_content("You have added #{landmark2.name} to your tour list.")
+      expect(page).to have_content('You have 2 landmarks in your tour list.')
+
+      visit landmark_path(landmark3)
+
+      click_button 'Add to Tour List'
+
+      expect(page).to have_content("You have added #{landmark3.name} to your tour list.")
+      expect(page).to have_content('You have 3 landmarks in your tour list.')
     end
 
-    it "displays the total number of landmarks in the tour list" do
-      visit landmark_path(@landmark1)
+    scenario 'displays the total number of landmarks in the tour list' do
+      visit landmark_path(landmark1)
 
-      expect(page).to have_content("Create Tour(0)")
+      expect(page).to have_content('Create Tour(0)')
 
-      click_button "Add to Tour List"
+      click_button 'Add to Tour List'
 
-      expect(page).to have_content("Create Tour(1)")
+      expect(page).to have_content('Create Tour(1)')
 
-      visit landmark_path(@landmark2)
-      click_button "Add to Tour List"
+      visit landmark_path(landmark2)
 
-      expect(page).to have_content("Create Tour(2)")
+      click_button 'Add to Tour List'
 
-      visit landmark_path(@landmark3)
-      click_button "Add to Tour List"
+      expect(page).to have_content('Create Tour(2)')
 
-      expect(page).to have_content("Create Tour(3)")
+      visit landmark_path(landmark3)
+
+      click_button 'Add to Tour List'
+
+      expect(page).to have_content('Create Tour(3)')
+    end
+  end
+
+  context 'as a admin'
+
+  context 'edge case' do
+    scenario 'unable to add same landmark' do
+      visit landmark_path(landmark1)
+
+      click_button 'Add to Tour List'
+
+      expect(page).to have_content("You have added #{landmark1.name} to your tour list.")
+      expect(page).to have_content('You have 1 landmarks in your tour list.')
+
+      visit landmark_path(landmark1)
+
+      click_button 'Add to Tour List'
+
+      expect(page).to have_content("You already have #{landmark1.name} in your tour list")
     end
   end
 end
--- spec/models/tour_list_spec.rb
+++ spec/models/tour_list_spec.rb
@@ -1,46 +1,47 @@
-require 'rails_helper'
+require 'spec_helper'
+require './app/models/tour_list'
 
-RSpec.describe TourList do
+RSpec.describe TourList, type: :model do
 
+  let(:subject) { TourList.new({'1' => 1, '2' => 1}) }
   let(:landmark1) { create(:landmark) }
   let(:landmark2) { create(:landmark) }
   let(:tour) { TourList.new({"#{landmark1.id}" => 1, "#{landmark2.id}" => 1}) }
-  let(:subject) { TourList.new({"1" => 1, "2" => 1}) }
 
-  describe "#total_count" do
-    it "calculates the total number of landmarks it holds" do
+  describe '#total_count' do
+    it 'calculates the total number of landmarks it holds' do
       expect(subject.total_count).to eq(2)
     end
   end
 
-  describe "#count_of" do
-    it "returns the count of all landmarks in the cart" do
+  describe '#count_of' do
+    it 'returns the count of all landmarks in the cart' do
       expect(subject.count_of(1)).to eq(1)
     end
   end
 
-  describe "#add_landmark" do
-    it "adds a landmark to its contents" do
+  describe '#add_landmark' do
+    it 'adds a landmark to its contents' do
       subject.add_landmark(3)
 
       expect(subject.contents).to eq({'1' => 1, '2' => 1, '3' => 1})
     end
 
-    it "unable to increase contents greater than one" do
+    it 'unable to increase contents greater than one' do
       subject.add_landmark(2)
 
       expect(subject.contents).to eq({'1' => 1, '2' => 1})
     end
   end
 
-  describe "#remove_landmark" do
-    it "removes a landmark to its contents" do
+  describe '#remove_landmark' do
+    it 'removes a landmark to its contents' do
       subject.remove_landmark(2)
 
       expect(subject.contents).to eq({'1' => 1})
     end
 
-    it "unable to decrease contents less than zero" do
+    it 'unable to decrease contents less than zero' do
       subject.remove_landmark(3)
 
       expect(subject.contents).to_not eq({'1' => 1, '2' => 1, '3' => -1})
@@ -48,9 +49,9 @@ RSpec.describe TourList do
     end
   end
 
-  describe "#load_landmark" do
-    it "loads landmarks in an array" do
-      expect(tour.landmark).to eq([landmark1, landmark2])
+  describe '#load_landmarks' do
+    it 'loads landmarks in an array' do
+      expect(tour.landmarks).to eq([landmark1, landmark2])
     end
   end
 end
