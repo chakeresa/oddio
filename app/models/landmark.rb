c64346a3fc440f5e9de0913f2b991f0a2832024b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-31 20:37:12 -0600

    Add Landmark#total_score

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -7,6 +7,13 @@ class Landmark < ApplicationRecord
 
   before_save :update_md5_hash
 
+  # TODO: abstract into a Votable module & include in several classes
+  def total_score
+    get_score
+    score = JSON.parse(get_score.body)["data"]["attributes"]["total_score"]
+    return score
+  end
+
   private
 
   def update_md5_hash
@@ -19,5 +26,18 @@ class Landmark < ApplicationRecord
       #{self.website}
       #{self.photo_reference}"
     )
+  end
+
+  # TODO: abstract methods below into to service (in several places)
+  def votes_service
+    Faraday.new url: "https://votes-app-1903.herokuapp.com" do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_score
+    votes_service.get do |req|
+      req.url "/api/v1/landmark/#{self.id}/score"
+    end
   end
 end
