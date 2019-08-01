45335deea9ae9c1d715a21c851e118935b6de365
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-31 21:07:24 -0600

    Refactor VoteService

--- app/controllers/user/votes_x_controller.rb
+++ app/controllers/user/votes_x_controller.rb
@@ -1,6 +1,6 @@
 class User::VotesXController < ApplicationController
   def create
-    response = request_create
+    response = VoteService.new(vote_service_params).request_create
     hash = JSON.parse(response.body, symbolize_names: true)
     total_score = hash[:data][:attributes][:total_score]
     render json: { new_score: total_score }
@@ -8,17 +8,12 @@ class User::VotesXController < ApplicationController
 
   private
 
-  # TODO: abstract methods below into votes api service
-  def conn
-    Faraday.new url: "https://votes-app-1903.herokuapp.com" do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
-  end
-
-  def request_create
-    rating = params["type"] == "upvote" ? 1 : -1
-    request = conn.post do |req|
-      req.url "/api/v1/#{params[:votable_type]}/#{params[:votable_id]}/create_vote/#{current_user.vote_token}/#{rating}"
-    end
+  def vote_service_params
+    {
+      votable_type: params[:votable_type],
+      votable_id: params[:votable_id],
+      type: params[:type],
+      vote_token: current_user.vote_token
+    }
   end
 end
