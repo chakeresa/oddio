4767c468a32a76bbe1966c7e0a13861c599a226f
lpile <logan.pile@gmail.com>
2019-08-03 10:20:39 -0600

    Merge from master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_180717) do
+ActiveRecord::Schema.define(version: 2019_07_25_022142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,17 +34,17 @@ ActiveRecord::Schema.define(version: 2019_07_24_180717) do
   end
 
   create_table "landmarks", force: :cascade do |t|
+    t.string "name"
     t.decimal "lat", precision: 10, scale: 6
     t.decimal "long", precision: 10, scale: 6
-    t.string "name"
     t.string "address"
     t.string "phone_number"
     t.string "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "place_id"
     t.string "website"
     t.string "photo_reference"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "md5_hash"
   end
 
