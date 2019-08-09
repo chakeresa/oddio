1d10f192f1b4719ffb01a3ceb0ddd724bc74de99
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-09 16:54:48 -0600

    Add ApplicationRecord::sort and VoteService#sort
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.sort(votable_type, collection)
+    array_of_ids = collection.pluck(:id)
+    parameters = {
+      votable_type: votable_type,
+      array_of_ids: array_of_ids
+    }
+    hash = VoteService.new(parameters).sort
+    # TODO: map back to objects
+  end
 end
