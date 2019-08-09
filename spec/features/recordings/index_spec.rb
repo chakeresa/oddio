32a8eaeb11194dd7db569e7e56959252f5a51c63
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-09 16:37:20 -0600

    Add feature test for sorted recordings index
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

--- spec/features/recordings/index_spec.rb
+++ spec/features/recordings/index_spec.rb
@@ -1,13 +1,10 @@
 require 'rails_helper'
 
-feature 'recordings index page', :vcr do
-  before(:each) do
-    @recordings = create_list(:recording, 2)
-  end
-
+feature 'recordings index page' do
   describe 'as a visitor' do
     it 'shows all recordings' do
       VCR.use_cassette('visitor_sees_recording', record: :new_episodes) do
+        @recordings = create_list(:recording, 2)
         visit recordings_path
         expect(page).to have_content('All Recordings')
         expect(page.all('.recording-list').count).to eq(2)
@@ -22,18 +19,52 @@ feature 'recordings index page', :vcr do
       end
     end
 
-    it 'the vote arrows dont display' do
-      visit recordings_path
+    it 'sorts recordings by total rating from Sinatra' do
+      WebMock.allow_net_connect!
+      VCR.turn_off!
+      # VCR.use_cassette('sorted_recordings_index', record: :new_episodes) do
+        r1, r2, r3 = create_list(:recording, 3)
+        votable_type = 'recording'
+        # r2 rating = +1, r1 rating = 0, r3 rating = -1
+        VoteService.new(votable_type: votable_type, votable_id: r2.id, type: 'upvote', vote_token: '12345').request_create
+        VoteService.new(votable_type: votable_type, votable_id: r3.id, type: 'downvote', vote_token: '12345').request_create
 
+        visit recordings_path
+
+        expect(page.all('.recording-list').count).to eq(3)
+
+        within(first('.recording-list')) do
+          expect(page).to have_content(r2.title)
+          expect(page).to have_content(1)
+        end
+
+        within(second('.recording-list')) do
+          expect(page).to have_content(r1.title)
+          expect(page).to have_content(0)
+        end
+
+        within(third('.recording-list')) do
+          expect(page).to have_content(r3.title)
+          expect(page).to have_content(-1)
+        end
+      # end
+    end
+
+    it 'the vote arrows dont display' do
+      @recordings = create_list(:recording, 2)
+      
+      visit recordings_path
+      
       expect(page).to_not have_content('▲')
       expect(page).to_not have_content('▼')
     end
   end
-
+  
   describe 'as a user' do
     let(:user) { create(:user) }
-
+    
     it 'the vote arrows display' do
+      @recordings = create_list(:recording, 2)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit recordings_path
