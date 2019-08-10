f0e00cc3ca2ac4084294c9725258cd57d1c843df
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-09 18:28:41 -0600

    Add cassettes for recordings index sort
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

--- spec/features/recordings/index_spec.rb
+++ spec/features/recordings/index_spec.rb
@@ -20,10 +20,10 @@ feature 'recordings index page' do
     end
 
     it 'sorts recordings by total rating from Sinatra' do
-      WebMock.allow_net_connect!
-      VCR.turn_off!
-      # VCR.use_cassette('sorted_recordings_index', record: :new_episodes) do
-        r1, r2, r3 = create_list(:recording, 3)
+      VCR.use_cassette('sorted_recordings_index', record: :new_episodes) do
+        r1 = create(:recording, id: 999 + rand(9999))
+        r2 = create(:recording, id: 999 + rand(9999))
+        r3 = create(:recording, id: 999 + rand(9999))
         votable_type = 'recording'
         # r2 rating = +1, r1 rating = 0, r3 rating = -1
         VoteService.new(votable_type: votable_type, votable_id: r2.id, type: 'upvote', vote_token: '12345').request_create
@@ -47,16 +47,18 @@ feature 'recordings index page' do
           expect(page).to have_content(r3.title)
           expect(page).to have_content(-1)
         end
-      # end
+      end
     end
 
     it 'the vote arrows dont display' do
-      @recordings = create_list(:recording, 2)
-      
-      visit recordings_path
-      
-      expect(page).to_not have_content('▲')
-      expect(page).to_not have_content('▼')
+      VCR.use_cassette('recordings_index_page/as_a_visitor/the_vote_arrows_dont_display', record: :new_episodes) do
+        @recordings = create_list(:recording, 2)
+        
+        visit recordings_path
+        
+        expect(page).to_not have_content('▲')
+        expect(page).to_not have_content('▼')
+      end
     end
   end
   
@@ -64,13 +66,15 @@ feature 'recordings index page' do
     let(:user) { create(:user) }
     
     it 'the vote arrows display' do
-      @recordings = create_list(:recording, 2)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      VCR.use_cassette('recordings_index_page/as_a_user/the_vote_arrows_display', record: :new_episodes) do
+        @recordings = create_list(:recording, 2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit recordings_path
+        visit recordings_path
 
-      expect(page).to have_content('▲')
-      expect(page).to have_content('▼')
+        expect(page).to have_content('▲')
+        expect(page).to have_content('▼')
+      end
     end
   end
 end
