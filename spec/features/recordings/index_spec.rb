d9627b53531f0d96b99c1577e8b6d243a5cb32c3
Patrick Duvall <duvallp208@gmail.com>
2019-08-13 17:20:49 -0600

    Move flag test to recordigns index

--- spec/features/recordings/index_spec.rb
+++ spec/features/recordings/index_spec.rb
@@ -28,6 +28,16 @@ feature 'recordings index page', :vcr do
       expect(page).to_not have_content('▲')
       expect(page).to_not have_content('▼')
     end
+
+    it "has a button to flag content" do
+      visit recordings_path(@r1)
+      expect(@r1.flags.count).to eq(0)
+      click_button("⚑")
+      expect(@r1.flags.count).to eq(1)
+      click_button("⚑")
+      # expect(page).to have_content("Thank you for your re
+      expect(@r1.flags.count).to eq(1)
+    end
   end
 
   describe 'as a user' do
