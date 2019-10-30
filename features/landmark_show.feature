bdc9c4fae8dc60e738425a556181d3cfab797001
Alexandra Chakeres <alexchakeres@gmail.com>
2019-10-29 20:43:24 -0600

    Add landmark_show.feature & step def skeletons

---
+++ features/landmark_show.feature
@@ -0,0 +1,12 @@
+Feature: Landmark Show Page
+  feature tests of the landmark show page
+
+  Scenario Outline: Shows add recording button (or not)
+    Given a generic landmark
+    Given I'm a <user_type>
+    When I visit that landmark's show page
+    Then I <should_or_should_not> see the "Upload new recording" button
+
+    Examples:
+    | user_type   | should_or_should_not |
+    | visitor     | should not           |
---
+++ features/step_definitions/step_defs.rb
@@ -0,0 +1,15 @@
+Given("a generic landmark") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I'm a visitor") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I visit that landmark's show page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should not see the {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
